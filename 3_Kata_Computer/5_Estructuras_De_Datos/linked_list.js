bf3277e542f791296e9092d74c31453152f02b7c
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-09-22 21:12:53 -0500

    agregar a nuevos nodos

--- 3_Kata_Computer/5_Estructuras_De_Datos/linked_list.js
+++ 3_Kata_Computer/5_Estructuras_De_Datos/linked_list.js
@@ -1,44 +1,60 @@
-/* const lista = {
-    head: "loquesea",
-    length: 0
-
-} */
-
-//El nodo es la información del elemento y su apuntador (o enlace) al siguiente elemento.
-
-class Node {
-    constructor(data) {
-        //en data guardamos cualquier objeto que queremos
+// El nodo es la información del elemento y su apuntador(o enlace) al siguiente elemento. 
+class Node{
+    constructor(data){
+        // en data guardamos cualquier objeto que queremos
         this.data = data;
-        //es quien nos indica el siguiente nodo, por defecto es null
+        // es quien nos indica el siguiente nodo, por defecto esta es nulo.
         this.next = null;
     }
 }
 
-//Linked list
-//Una lista ligada debe de tener una cabecera (head)
-//Nos va a indicar el inicio de la lista.
-//El tamaño de la lista (length)
+// Linked list
 
-//Las listas tienen dos comportamientos esenciales,
-//insertar (add)
-//borrar (delete)
+// Una lista ligada debe de tener una cabecera (head)
+// Nos va a indicar el inicio de la lista.
+
+// El tamaño de la lista (length)
+
+// Las listas tienen dos comportamientos esenciales,
+// insertar (add)
+// borrar (delete)
 
 class LinkedList{
+    // estas son las propiedades de mi lista
     constructor() {
         this.head = null;
         this.length = 0;
     }
-
-//estos son los métodos o comportamientos de mi lista
-
-    add(data) {
-        const node = new Node(data); //{data: data, next : null}
+    // estos son los métodos o comportamientos de mi lista
+    add(data){
+        console.log('add ' + data);
+       const node = new Node(data); // { data: data, next : null }
+       if(this.head === null) {
         this.head = node;
-        this.leng++;
-}
+       } else { // en este else ya sabeos que existe this.head;
+            let currentNode = this.head;
+            // while: mientras (condicion: se cumpla la condicion) { hace esto }
+            while(currentNode.next) { // verifico si next tiene un nodo
+                currentNode = currentNode.next; // sustituyo el node que vive en next a mi nodo actual
+            }
+            // cuando next sea nulo, entonces sale del while y continua con el resto del codigo
+            currentNode.next = node;
+       }
+       
+       this.length++;
+    }
 }
 
 const listaDeAlumnos = new LinkedList();
-listaDeAlumnos.add('Valeria');
+
+listaDeAlumnos.add('Cesar');
+
+listaDeAlumnos.add('Juan Camilo');
+
+listaDeAlumnos.add('Marlon');
+
+listaDeAlumnos.add('Ulma');
+ 
+listaDeAlumnos.add('Margarito');
+
 console.log(listaDeAlumnos);
