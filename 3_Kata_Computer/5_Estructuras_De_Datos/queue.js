8bdb09ceae1aee48053cb7890fbe12090ff5b7b9
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-09-27 21:39:53 -0500

    update

---
+++ 3_Kata_Computer/5_Estructuras_De_Datos/queue.js
@@ -0,0 +1,16 @@
+//TODO: ver ejemplos de una pila
+const pila = [];
+//PUSH: Agregar elementos
+
+/* console.log(pila.push('Federico'));
+console.log(pila.push('Alfredo'));
+console.log(pila.push('Alexis'));
+
+console.log(pila);
+
+console.log(pila.pop());
+console.log(pila);
+console.log(pila.pop());
+console.log(pila); */
+
+//TODO: Implementación de la pila.
