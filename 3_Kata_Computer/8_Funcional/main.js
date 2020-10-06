0b33244c3df10a830f4d93e879f31fd51ce82eea
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-10-05 20:24:14 -0500

    programacion funcional

---
+++ 3_Kata_Computer/8_Funcional/main.js
@@ -0,0 +1,41 @@
+//Pure function
+const sumaUnoAlNumero = (numero) => numero + 1;
+//console.log(sumaUnoAlNumero(7));
+
+//Avoid side effect / Esta función tiene un efecto secundario
+const sumaNumeroRandom = (numero) => numero + Math.random();
+//console.log(sumaNumeroRandom(7));
+
+var temperatura = 21;
+const dimeTemperatura = () => `La temperatura es ${temperatura}`;
+//console.log(dimeTemperatura());
+
+//Function composition
+const realizaOperacion = (a, b, operacion) => operacion(a, b);
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const mult = (a, b) => a * b;
+
+/* console.log(realizaOperacion(5, 3, suma));
+console.log(realizaOperacion(10, 7, resta));
+console.log(realizaOperacion(10, 7, mult)); */
+
+//Avoid mutating state
+const sensei = Object.freeze ({//object.freeze no me permite cambiar el dato del objeto
+
+    nombre: 'Gera',
+  programa: 'Master en Coding',
+  equipo: Object.freeze({
+    modelo: 'ASUS',
+    ram: 12
+  })
+});
+
+sensei.nombre = 'Mali';
+sensei.programa = 'Cinta Roja';
+sensei.equipo.modelo = 'Macbook';
+sensei.equipo.ram = 6;
+
+
+console.log(sensei);
