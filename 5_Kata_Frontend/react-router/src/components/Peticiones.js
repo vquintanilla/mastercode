cba89e2d90f7eabd89678fe288d8aeb852e465ab
vquintnilla <vquintnilla@dinersclub.com.ec>
2021-02-10 22:14:25 -0500

    Mostrar información del taco en un componente

--- 5_Kata_Frontend/react-router/src/components/Peticiones.js
+++ 5_Kata_Frontend/react-router/src/components/Peticiones.js
@@ -1,33 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import TacoComponent from './TacoComponent';
+
 export default function Peticiones() {
   const [taco, setTaco] = useState({});
-
-  /*
-    useEffect (escrito así) se ejecuta siempre
-    después de renderizar (retornar) nuestro
-    componente
-  */
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setTexto("Pasaron 3 segundos!!!");
-  //   }, 3000)
-  // })
-
-  /*
-    useEffect (escrito usando DEPENDENCIAS [])
-    se ejecuta UNA SOLA VEZ al cargar el componente
-    esto es el equivalente a
-    componentDidMount
-  */
-  // useEffect(
-  //   () => {
-  //     console.log("Hola desde useEffect :}")
-  //   },
-  //   [],
-  // );
-
   const tacoAPI = 'http://taco-randomizer.herokuapp.com/random/';
 
   async function getTaco() {
@@ -48,11 +25,7 @@ export default function Peticiones() {
       <h1>Peticiones</h1>
       <h2>useEffect</h2>
       <h3>- TacoFancy API -</h3>
-      <p> {
-        taco.condiment
-          ? taco.condiment.name
-          : 'Estamos eligiendo tu taco ideal...'
-      } </p>
+      <TacoComponent taco={taco}/>
     </>
   )
 }
