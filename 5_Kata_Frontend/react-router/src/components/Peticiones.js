42339ff05b765a5ab3b2a94a738a09531e730530
vquintnilla <vquintnilla@dinersclub.com.ec>
2021-02-10 22:41:31 -0500

    Instalar prop-types para agregar validaciones al TacoComponent

--- 5_Kata_Frontend/react-router/src/components/Peticiones.js
+++ 5_Kata_Frontend/react-router/src/components/Peticiones.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import TacoComponent from './TacoComponent';
 
@@ -31,4 +32,8 @@ export default function Peticiones() {
       }
     </>
   )
+}
+
+TacoComponent.prototype={
+  taco: PropTypes.object,
 }
